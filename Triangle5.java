15d3d3150120728175d38661b19ac93109166239
monikamr1229 <mahantesh0917@gmail.com>
2023-02-07 18:51:28 +0530

    Stage

---
+++ Triangle5.java
@@ -0,0 +1,15 @@
+class Triangle5
+{
+	void Trianglea()
+	{
+		int b=2;
+		int h=3;
+		double area=(1/2.0)*b*h;
+		System.out.println(area);
+
+	}
+	public static void main(String[] args)
+	{
+		new Triangle5().Trianglea();
+	}
+}
