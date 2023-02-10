6decd088066229a39eda6ebd2ff485cc76e8878a
dhineshgits <124063062+dhineshgits@users.noreply.github.com>
2023-02-10 18:12:22 +0530

    Update Abc.java
    merge merge

--- Abc.java
+++ Abc.java
@@ -2,6 +2,13 @@ class  Abc
 {
 	public static void main(String[] args) 
 	{
-		Def.add();
+		add();
 	}
+
+static void add(){
+	int a = 10;
+	int b = 20;
+	int c = a+b;
+	System.out.println(c);
+}
 }
